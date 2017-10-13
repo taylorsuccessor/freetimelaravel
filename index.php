2e7f1cafdcceae8351c3f4e8230a52bd1137b842
taylorsuccessor <taylorsuccessor@gmail.com>
2017-10-13 10:56:17 +0300

    init home page design

---
+++ index.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Free Time Sharing</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="css/main.css" rel="stylesheet" type="text/css" />
+
+
+</head>
+<body>
+
+<div class="page">
+
+    <header >
+        <div class="mainHeaderContainer">
+
+            <div class="mainHeaderInnerContainer">
+
+                <div id="mainLogoContainer">
+                    <h2>Free Time Sharing</h2>
+                </div>
+            </div>
+        </div>
+
+    </header>
+
+
+
+
+    <div class="innerPageContainer">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div class="pageAside">
+
+
+            <div class="leftContainer box">
+
+
+                <div class="tabContainer">
+                    <div class="tab active"><label for="post_type_0"><input type="radio" name="type" id="post_type_0" checked > Schedule</label></div>
+                    <div class="tab"><label for="post_type_1"><input type="radio" name="type" id="post_type_1" >notification</label></div>
+
+                </div>
+
+
+                <div class="list " id="scheduleList">
+                    <div class="listInnerContainer">
+
+                        <div class="listItem">
+
+
+
+
+                            <div class="smallPost box">
+                                <header>
+                                    <h3 class="postTitle">At <span class="green">1pm </span> Every Day <span>for 1 hour</span>  <span class="phone">0785181656</span> </h3>
+                                    <div class="headerTime decreasingTime ">still 1hour 2 minutes</div>
+                                </header>
+                                <section class="bostBody">
+                                    <div class="postMainImgDiv">
+                                        <img src="images/img1.jpg">
+                                    </div>
+                                    <div class="postDetailDiv"><p>
+                                            hi , it's moh and am free for today to drink cofe any one there
+                                        </p></div>
+
+
+                                </section>
+                            </div>
+
+
+
+
+                        </div>
+
+                    </div>
+                </div>
+
+
+
+                <div class="list " id="notificationList">
+                    <div class="listInnerContainer">
+
+                        <div class="listItem">
+
+
+
+
+                            <div class="oneListNotification callMeNotification box">
+                                <header>
+                                    <h3  > Call Me <span class="phone">0785181656</span> </h3>
+                                    <div class="headerTime increasingTime ">from 1hour 2 minutes</div>
+                                </header>
+                                <section class="notificationBody">
+
+                                   <p>
+                                            hi , it's moh and am free for today to drink cofe any one there
+                                        </p>
+
+
+                                </section>
+                            </div>
+
+
+
+
+                        </div>
+
+                    </div>
+                </div>
+
+
+
+
+
+            </div>
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div class="pageCenterContent">
+
+            <div class="post box">
+                <header>
+                    <h3 class="postTitle">At <span class="green">1pm </span> Every Day <span>for 1 hour</span> <span class="phone">0785181656</span> </h3>
+                    <div class="headerTime decreasingTime ">still 1hour 2 minutes</div>
+                </header>
+                <section class="bostBody">
+                    <div class="postMainImgDiv">
+                        <img src="images/img1.jpg">
+                    </div>
+                    <div class="postDetailDiv"><p>
+                            hi , it's moh and am free for today to drink cofe any one there
+                        </p></div>
+
+                    <div class="addressDiv">
+                        <p class="address">Amman, Swilih Abdullah street
+                            <a class="addressMapLink"><i class="fa fa-map"></i> show map </a>
+                        </p>
+                    </div>
+
+                </section>
+                <footer>
+
+
+
+                    <div class="postButtonsDiv">
+                        <div class="buttonContainer">
+                            <a href="#" class="callMeLink"> <i class="fa fa-users"></i> Call Me (<span>23</span>) </a>
+                        </div>
+                        <div class="buttonContainer">
+                            <a href="#" class="amFreeLink"> <i class="fa fa-users"></i>Am Free (<span>23</span>) </a>
+                        </div>
+                        <div class="buttonContainer">
+                            <a href="#" class="followLink"> <i class="fa fa-users"></i> Follow (<span>23</span>) </a>
+                        </div>
+
+                    </div>
+
+
+
+
+                    <div class="allCommentsDiv">
+                        <div class="commentsInnerDiv">
+
+                            <div class="oneCommentDiv"><h6>0785181656</h6><p>this is my comment</p><span class="increasingTime">1hour 2 minutes</span> </div>
+                            <div class="oneCommentDiv"><h6>0785181656</h6><p>this is my comment</p><span class="increasingTime">1hour 2 minutes</span> </div>
+                            <div class="oneCommentDiv"><h6>0785181656</h6><p>this is my comment</p><span class="increasingTime">1hour 2 minutes</span> </div>
+
+                        </div>
+
+                        <div class="postCommentDiv">
+                            <form method="post" class="postCommentForm"><input name="comment" placeholder="Comment" class="postCommentInput"/></form>
+                        </div>
+                    </div>
+
+                </footer>
+            </div>
+
+
+        </div>
+
+
+        <div class="pageAside">
+
+
+            <div class="addFreeTimeFormContainer box">
+
+
+                <div class="tabContainer">
+                    <div class="tab active"><label for="post_type_0"><input type="radio" name="type" id="post_type_0" checked > Add free Time</label></div>
+                    <div class="tab"><label for="post_type_1"><input type="radio" name="type" id="post_type_1" >I need some one</label></div>
+
+                </div>
+
+
+                <form  class="addFreeTimeForm" >
+
+                    <div class="inputContainer" id="inputContainerTime">
+                        <select ><option>free Time</option><option>1 AM</option></select>
+                    </div>
+
+
+                    <div class="inputContainer" id="inputContainerPeriod">
+                        <select ><option>For</option><option>rest of day</option></select>
+                    </div>
+
+
+                    <div class="inputContainer"  id="inputContainerInterval" >
+                        <select ><option>Every day</option><option>Every Week</option></select>
+                    </div>
+
+
+                    <div class="inputContainer" id="inputContainerStart">
+                        <input placeholder="Start From" >
+                    </div>
+
+
+                    <div class="inputContainer" id="inputContainerDetail">
+                        <textarea placeholder="What you are doing in your free time" ></textarea>
+                    </div>
+
+                    <hr>
+
+                    <div class="inputContainer withIcon"  id="inputContainerAddress">
+                        <input placeholder="Address" >
+                        <a href="#" class="icon fa fa-map"></a>
+                    </div>
+
+
+                    <div class="inputContainer"  id="inputContainerPhone">
+                        <input placeholder="phone" >
+                    </div>
+
+                    <div class="inputContainer"  id="inputContainerSubmit">
+                        <button type="submit"   >Share Free Time</button>
+                    </div>
+
+
+                </form>
+            </div>
+
+
+        </div>
+
+
+
+    </div>
+
+
+
+
+</div>
+
+</body>
+</html>
